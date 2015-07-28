fbb6dc32b072b44c44e4c73a25409f303874ca61
Bryan-Burkett <crazydude008@gmail.com>
2015-07-28 12:06:25 -0500

    fixed python REST for basic authentication

--- src/python_rest_HelloWorld.py
+++ src/python_rest_HelloWorld.py
@@ -1,6 +1,6 @@
-##
+
 # Python Sample Application: Connection to Informix using REST
-##
+
 
 # Topics
 # 1 Inserts
@@ -19,20 +19,20 @@
 import json
 import os
 import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.poolmanager import PoolManager
-import ssl
 from flask import Flask, render_template
 
 app = Flask(__name__)
-url = ""
-database = ""
+baseUrl = "http://username:password@yoururl:port"
+database = "dbTest"
 port = int(os.getenv('VCAP_APP_PORT', 8080))
+baseDbUrl= baseUrl + "/" + database
+collectionName = "pythonREST"
+commands = []
 
 # parsing vcap services
-def parseVCAP():
-    global database
-    global url
+# def parseVCAP():
+#     global database
+#     global url
 #     altadb = json.loads(os.environ['VCAP_SERVICES'])['altadb-dev'][0]
 #     credentials = altadb['credentials']
 #     ssl = False
@@ -40,39 +40,17 @@ def parseVCAP():
 #         url = credentials['ssl_rest_url']
 #     else:
 #         url = credentials['rest_url']
-    
-    
-# certfile = ''
-class MyAdapter(HTTPAdapter):
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = PoolManager(num_pools=connections,
-                                       maxsize=maxsize,
-                                       block=block
-#                                        ssl_version=ssl.PROTOCOL_TLSv1,
-#                                        ca_certs = certfile
-                                       )
          
-# baseUrl="https://yourURL:port"
-# dbname="db"
-# baseDbUrl= url + "/" + database
-authInfo=('cloudqa','cloud4pass')
-collectionName = "pythonREST"
-
-commands = []
-
 def printError(message, reply):
     commands.append("Error: " + message)
     commands.append("status code: " + str(reply.status_code))
     commands.append("content: " + str(reply.content))
     
 def doEverything():
-    session = requests.Session()
-    session.mount('http://', MyAdapter())
-    baseDbUrl= url + "/" + database
     commands.append("# 1 Inserts")
     commands.append( "# 1.1 Insert a single document to a collection")
     data = json.dumps({'name': 'test1', 'value': 1})
-    reply = session.post(baseDbUrl + "/" + collectionName, data, auth=authInfo)
+    reply = requests.post(baseDbUrl + "/" + collectionName, data)
     if reply.status_code == 200:
         doc = reply.json()
         commands.append("inserted " + str(doc.get('n')) + " documents")
@@ -81,7 +59,7 @@ def doEverything():
     
     commands.append("# 1.2 Insert multiple documents to a collection")
     data = json.dumps([{'name': 'test1', 'value': 1}, {'name': 'test2', 'value': 2}, {'name': 'test3', 'value': 3} ] )
-    reply = session.post(baseDbUrl + "/" + collectionName, data)
+    reply = requests.post(baseDbUrl + "/" + collectionName, data)
     if reply.status_code == 202:
         doc = reply.json()
         commands.append("inserted " + str(doc.get('n')) + " documents")
@@ -91,7 +69,7 @@ def doEverything():
     commands.append("# 2 Queries")
     commands.append("# 2.1 Find a document in a collection that matches a query condition")
     query = json.dumps({'name':'test1'})
-    reply = session.get(baseDbUrl + "/" + collectionName + "?query=" + query)
+    reply = requests.get(baseDbUrl + "/" + collectionName + "?query=" + query)
     if reply.status_code == 200:
         doc = reply.json()
         commands.append("query result: " + str(doc[0]))
@@ -100,42 +78,42 @@ def doEverything():
           
     commands.append("# 2.2 Find all documents in a collection that match a query condition")
     query = json.dumps({'name':'test1'})
-    reply = session.get(baseDbUrl + "/" + collectionName + "?query=" + query)
+    reply = requests.get(baseDbUrl + "/" + collectionName + "?query=" + query)
     if reply.status_code == 200:
         doc = reply.json()
         commands.append("query result: " + str(doc))
     else:
         printError("Unable to query documents in collection", reply)
-      
-    commands.append("# 2.3 Find all documents in a collection")
-    reply = session.get(baseDbUrl+ "/" + collectionName)
+                             
+#     commands.append("# 2.3 Find all documents in a collection")
+    reply = requests.get(baseDbUrl+ "/" + collectionName)
     if reply.status_code == 200:
         doc = reply.json()
         commands.append("query result: " + str(doc))
     else:
         printError("Unable to query documents in collection", reply)
-      
+                             
     commands.append("# 3 Update documents in a collection")
     query = json.dumps({'name': 'test3'})
     data = json.dumps({'$set' : {'value' : 9} })
-    reply = session.put(baseDbUrl + "/" + collectionName + "?query=" + query, data)
+    reply = requests.put(baseDbUrl + "/" + collectionName + "?query=" + query, data)
     if reply.status_code == 200:
         doc = reply.json()
         commands.append("updated " + str(doc.get('n')) + " documents")
     else:
         printError("Unable to update documents in collection", reply)
-      
+                             
     commands.append("# 4 Delete documents in a collection")
     query = json.dumps({'name': 'test2'})
-    reply = session.delete(baseDbUrl + "/" + collectionName + "?query=" + query)
+    reply = requests.delete(baseDbUrl + "/" + collectionName + "?query=" + query)
     if reply.status_code == 200:
         doc = reply.json()
         commands.append("deleted " + str(doc.get('n')) + " documents")
     else:
         printError("Unable to delete documents in collection", reply)
-      
+                             
     commands.append("# 5 Get a listing of collections")
-    reply = session.get(baseDbUrl)
+    reply = requests.get(baseDbUrl)
     if reply.status_code == 200:
         doc = reply.json()
         dbList = ""
@@ -146,7 +124,7 @@ def doEverything():
         printError("Unable to retrieve collection listing", reply)
           
     commands.append("# 6 Drop a collection")
-    reply = session.delete(baseDbUrl + "/" + collectionName)
+    reply = requests.delete(baseDbUrl + "/" + collectionName)
     if reply.status_code == 200:
         doc = reply.json()
         commands.append("delete collection result: " + str(doc))
@@ -161,7 +139,7 @@ def displayPage():
 def printCommands():
     global commands
     commands = []
-    parseVCAP()
+#     parseVCAP()
     doEverything()
     return render_template('tests.html', commands=commands)
  
