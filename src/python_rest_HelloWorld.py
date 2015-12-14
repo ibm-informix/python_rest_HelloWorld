88bd62325461f71667b5b780c0a447f404eb9e75
vonbarg <vonbarg@us.ibm.com>
2015-12-14 15:59:00 -0600

    Updates to improve error handling & clarity

--- src/python_rest_HelloWorld.py
+++ src/python_rest_HelloWorld.py
@@ -15,138 +15,159 @@
 # 5 Get a listing of collections
 # 6 Drop a collection
 
-import logging
 import json
+import logging
 import os
 import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-baseUrl = "http://username:password@yoururl:port/dbTest"
-port = int(os.getenv('VCAP_APP_PORT', 8080))
-collectionName = "pythonREST"
-commands = []
 
-# parsing vcap services
-def parseVCAP():
-    global database
-    global baseUrl
+# To run locally, set URL. The url should include the database name.
+# Otherwise, url and database information will be parsed from the Bluemix VCAP_SERVICES.
+URL = ""
+COLLECTION_NAME = "pythonREST"
+USE_SSL = False     # Set to True to use SSL url from VCAP_SERVICES
+SERVICE_NAME = os.getenv('SERVICE_NAME', 'timeseriesdatabase')
+port = int(os.getenv('VCAP_APP_PORT', 8080))
+
+def getDatabaseUrl():
+    """
+    Get database url
     
-    tsdb = json.loads(os.environ['VCAP_SERVICES'])['timeseriesdatabase'][0]
-    credentials = tsdb['credentials']
-    database = credentials['db']
-    ssl = False
-    if ssl == True:
-        baseUrl = credentials['rest_url_ssl']
-    else:
-        baseUrl = credentials['rest_url']
+    :returns: (url)
+    """
+    
+    # Use defaults
+    if URL :
+        return URL
+    
+    # Parse database info from VCAP_SERVICES
+    if (os.getenv('VCAP_SERVICES') is None):
+        raise Exception("VCAP_SERVICES not set in the environment")
+    vcap_services = json.loads(os.environ['VCAP_SERVICES'])
+    try:
+        tsdb = vcap_services[SERVICE_NAME][0]
+        credentials = tsdb['credentials']
+        if USE_SSL:
+            url = credentials['rest_url_ssl']
+        else:
+            url = credentials['rest_url']
+        return url
+    except KeyError as e:
+        logging.error(e)
+        raise Exception("Error parsing VCAP_SERVICES. Key " + str(e) + " not found.")
+
          
-def printError(message, reply):
-    commands.append("Error: " + message)
-    commands.append("status code: " + str(reply.status_code))
-    commands.append("content: " + str(reply.content))
+def printError(output, message, reply):
+    output.append("Error: " + message)
+    output.append("status code: " + str(reply.status_code))
+    output.append("content: " + str(reply.content))
     
 def doEverything():
-    commands.append("# 1 Inserts")
-    commands.append( "# 1.1 Insert a single document to a collection")
+    # Get database connectivity information
+    url = getDatabaseUrl()
+
+    output = []
+    output.append("# 1 Inserts")
+    output.append( "# 1.1 Insert a single document to a collection")
     data = json.dumps({'name': 'test1', 'value': 1})
-    reply = requests.post(baseUrl + "/" + collectionName, data)
+    reply = requests.post(url + "/" + COLLECTION_NAME, data)
     if reply.status_code == 200:
         doc = reply.json()
-        commands.append("inserted " + str(doc.get('n')) + " documents")
+        output.append("inserted " + str(doc.get('n')) + " documents")
     else:
-        printError("Unable to insert document", reply)
+        printError(output, "Unable to insert document", reply)
     
-    commands.append("# 1.2 Insert multiple documents to a collection")
+    output.append("# 1.2 Insert multiple documents to a collection")
     data = json.dumps([{'name': 'test1', 'value': 1}, {'name': 'test2', 'value': 2}, {'name': 'test3', 'value': 3} ] )
-    reply = requests.post(baseUrl + "/" + collectionName, data)
+    reply = requests.post(url + "/" + COLLECTION_NAME, data)
     if reply.status_code == 202:
         doc = reply.json()
-        commands.append("inserted " + str(doc.get('n')) + " documents")
+        output.append("inserted " + str(doc.get('n')) + " documents")
     else:
-        printError("Unable to insert multiple documents", reply)
+        printError(output, "Unable to insert multiple documents", reply)
     
-    commands.append("# 2 Queries")
-    commands.append("# 2.1 Find a document in a collection that matches a query condition")
+    output.append("# 2 Queries")
+    output.append("# 2.1 Find a document in a collection that matches a query condition")
     query = json.dumps({'name':'test1'})
-    reply = requests.get(baseUrl + "/" + collectionName + "?query=" + query)
+    reply = requests.get(url + "/" + COLLECTION_NAME + "?query=" + query)
     if reply.status_code == 200:
         doc = reply.json()
-        commands.append("query result: " + str(doc[0]))
+        output.append("query result: " + str(doc[0]))
     else:
-        printError("Unable to query documents in collection", reply)
+        printError(output, "Unable to query documents in collection", reply)
           
-    commands.append("# 2.2 Find all documents in a collection that match a query condition")
+    output.append("# 2.2 Find all documents in a collection that match a query condition")
     query = json.dumps({'name':'test1'})
-    reply = requests.get(baseUrl + "/" + collectionName + "?query=" + query)
+    reply = requests.get(url + "/" + COLLECTION_NAME + "?query=" + query)
     if reply.status_code == 200:
         doc = reply.json()
-        commands.append("query result: " + str(doc))
+        output.append("query result: " + str(doc))
     else:
-        printError("Unable to query documents in collection", reply)
+        printError(output, "Unable to query documents in collection", reply)
                              
-#     commands.append("# 2.3 Find all documents in a collection")
-    reply = requests.get(baseUrl+ "/" + collectionName)
+#     output.append("# 2.3 Find all documents in a collection")
+    reply = requests.get(url+ "/" + COLLECTION_NAME)
     if reply.status_code == 200:
         doc = reply.json()
-        commands.append("query result: " + str(doc))
+        output.append("query result: " + str(doc))
     else:
-        printError("Unable to query documents in collection", reply)
+        printError(output, "Unable to query documents in collection", reply)
                              
-    commands.append("# 3 Update documents in a collection")
+    output.append("# 3 Update documents in a collection")
     query = json.dumps({'name': 'test3'})
     data = json.dumps({'$set' : {'value' : 9} })
-    reply = requests.put(baseUrl + "/" + collectionName + "?query=" + query, data)
+    reply = requests.put(url + "/" + COLLECTION_NAME + "?query=" + query, data)
     if reply.status_code == 200:
         doc = reply.json()
-        commands.append("updated " + str(doc.get('n')) + " documents")
+        output.append("updated " + str(doc.get('n')) + " documents")
     else:
-        printError("Unable to update documents in collection", reply)
+        printError(output, "Unable to update documents in collection", reply)
                              
-    commands.append("# 4 Delete documents in a collection")
+    output.append("# 4 Delete documents in a collection")
     query = json.dumps({'name': 'test2'})
-    reply = requests.delete(baseUrl + "/" + collectionName + "?query=" + query)
+    reply = requests.delete(url + "/" + COLLECTION_NAME + "?query=" + query)
     if reply.status_code == 200:
         doc = reply.json()
-        commands.append("deleted " + str(doc.get('n')) + " documents")
+        output.append("deleted " + str(doc.get('n')) + " documents")
     else:
-        printError("Unable to delete documents in collection", reply)
+        printError(output, "Unable to delete documents in collection", reply)
                              
-    commands.append("# 5 Get a listing of collections")
-    reply = requests.get(baseUrl)
+    output.append("# 5 Get a listing of collections")
+    reply = requests.get(url)
     if reply.status_code == 200:
         doc = reply.json()
         dbList = ""
         for db in doc:
             dbList += "\'" + db + "\' "
-        commands.append("Collections: " + str(dbList))
+        output.append("Collections: " + str(dbList))
     else:
-        printError("Unable to retrieve collection listing", reply)
+        printError(output, "Unable to retrieve collection listing", reply)
           
-    commands.append("# 6 Drop a collection")
-    reply = requests.delete(baseUrl + "/" + collectionName)
+    output.append("# 6 Drop a collection")
+    reply = requests.delete(url + "/" + COLLECTION_NAME)
     if reply.status_code == 200:
         doc = reply.json()
-        commands.append("delete collection result: " + str(doc))
+        output.append("delete collection result: " + str(doc))
     else:
-        printError("Unable to drop collection", reply)
+        printError(output, "Unable to drop collection", reply)
+    
+    return output
 
 @app.route("/")
 def displayPage():
     return render_template('index.html')
 
 @app.route("/databasetest")
-def printCommands():
-    global commands
-    commands = []
+def runSample():
+    output = []
     try:
-        parseVCAP()
-        doEverything()
+        output = doEverything()
     except Exception as e:
         logging.exception(e) 
-        commands.append("EXCEPTION: " + str(e))
-        commands.append("See log for details")
-    return render_template('tests.html', commands=commands)
+        output.append("EXCEPTION (see log for details): " + str(e))
+    return render_template('tests.html', output=output)
  
 if (__name__ == "__main__"):
     app.run(host='0.0.0.0', port=port)
